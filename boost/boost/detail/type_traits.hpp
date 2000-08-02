fb6f5b818525fab04afca21d6cbf73efb355647d
johnmaddock <johnmaddock@localhost>
2000-08-02 10:31:48 +0000

    type traist update (first release)

--- boost/boost/detail/type_traits.hpp
+++ boost/boost/detail/type_traits.hpp
@@ -5,6 +5,9 @@
 //  warranty, and with no claim as to its suitability for any purpose.
 
 //  See http://www.boost.org for most recent version including documentation.
+//
+// misc traits classes that operate on or describe a type.
+// see libs/utility/type_traits.htm
 
 /* Release notes:
    31st July 2000:
