f9f48f33e580177cc14f6e8cf7b21e3ae48b96bb
agurtovoy <agurtovoy@localhost>
2002-09-14 07:17:33 +0000

    fixed bool traits for MSVC

--- boost/boost/type_traits/detail/bool_trait_def.hpp
+++ boost/boost/type_traits/detail/bool_trait_def.hpp
@@ -16,17 +16,25 @@
 
 // no include guards, the header is intended for multiple inclusion!
 
-#include "boost/type_traits/detail/bool_trait.hpp"
 #include "boost/type_traits/detail/template_arity_spec.hpp"
+#include "boost/mpl/bool_c.hpp"
 #include "boost/mpl/aux_/lambda_support.hpp"
 #include "boost/config.hpp"
 
+#if !defined(BOOST_MSVC) || BOOST_MSVC > 1200
+#   define BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) /**/
+#else
+#   define BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) \
+    typedef mpl::bool_c<C> base_; \
+    using base_::value; \
+    /**/
+#endif
+
 #define BOOST_TT_AUX_BOOL_TRAIT_DEF1(trait,T,C) \
 template< typename T > struct trait \
-    : ::boost::type_traits::bool_trait< C > \
+    : mpl::bool_c<C> \
 { \
-    typedef ::boost::type_traits::bool_trait< C > base_trait_; \
-    BOOST_STATIC_CONSTANT(bool, value = base_trait_::value); \
+    BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) \
     BOOST_MPL_AUX_LAMBDA_SUPPORT(1,trait,(T)) \
 }; \
 \
@@ -35,10 +43,9 @@ BOOST_TT_AUX_TEMPLATE_ARITY_SPEC(1,trait) \
 
 #define BOOST_TT_AUX_BOOL_TRAIT_DEF2(trait,T1,T2,C) \
 template< typename T1, typename T2 > struct trait \
-    : ::boost::type_traits::bool_trait< C > \
+    : mpl::bool_c<C> \
 { \
-    typedef ::boost::type_traits::bool_trait< C > base_trait_; \
-    BOOST_STATIC_CONSTANT(bool, value = base_trait_::value); \
+    BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) \
     BOOST_MPL_AUX_LAMBDA_SUPPORT(2,trait,(T1,T2)) \
 }; \
 \
@@ -47,58 +54,52 @@ BOOST_TT_AUX_TEMPLATE_ARITY_SPEC(2,trait) \
 
 #define BOOST_TT_AUX_BOOL_TRAIT_SPEC1(trait,sp,C) \
 template<> struct trait<sp> \
-    : ::boost::type_traits::bool_trait< C > \
+    : mpl::bool_c<C> \
 { \
-    typedef ::boost::type_traits::bool_trait< C > base_trait_; \
-    BOOST_STATIC_CONSTANT(bool, value = base_trait_::value); \
+    BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) \
     BOOST_MPL_AUX_LAMBDA_SUPPORT(1,trait,(sp)) \
 }; \
 /**/
 
 #define BOOST_TT_AUX_BOOL_TRAIT_SPEC2(trait,sp1,sp2,C) \
 template<> struct trait<sp1,sp2> \
-    : ::boost::type_traits::bool_trait< C > \
+    : mpl::bool_c<C> \
 { \
-    typedef ::boost::type_traits::bool_trait< C > base_trait_; \
-    BOOST_STATIC_CONSTANT(bool, value = base_trait_::value); \
+    BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) \
     BOOST_MPL_AUX_LAMBDA_SUPPORT(2,trait,(sp1,sp2)) \
 }; \
 /**/
 
 #define BOOST_TT_AUX_BOOL_TRAIT_PARTIAL_SPEC1_1(param,trait,sp,C) \
 template< param > struct trait<sp> \
-    : ::boost::type_traits::bool_trait< C > \
+    : mpl::bool_c<C> \
 { \
-    typedef ::boost::type_traits::bool_trait< C > base_trait_; \
-    BOOST_STATIC_CONSTANT(bool, value = base_trait_::value); \
+    BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) \
 }; \
 /**/
 
 #define BOOST_TT_AUX_BOOL_TRAIT_PARTIAL_SPEC1_2(param1,param2,trait,sp,C) \
 template< param1, param2 > struct trait<sp> \
-    : ::boost::type_traits::bool_trait< C > \
+    : mpl::bool_c<C> \
 { \
-    typedef ::boost::type_traits::bool_trait< C > base_trait_; \
-    BOOST_STATIC_CONSTANT(bool, value = base_trait_::value); \
+    BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) \
 }; \
 /**/
 
 #define BOOST_TT_AUX_BOOL_TRAIT_PARTIAL_SPEC2_1(param,trait,sp1,sp2,C) \
 template< param > struct trait<sp1,sp2> \
-    : ::boost::type_traits::bool_trait< C > \
+    : mpl::bool_c<C> \
 { \
-    typedef ::boost::type_traits::bool_trait< C > base_trait_; \
-    BOOST_STATIC_CONSTANT(bool, value = base_trait_::value); \
+    BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) \
     BOOST_MPL_AUX_LAMBDA_SUPPORT(2,trait,(sp1,sp2)) \
 }; \
 /**/
 
 #define BOOST_TT_AUX_BOOL_TRAIT_PARTIAL_SPEC2_2(param1,param2,trait,sp1,sp2,C) \
 template< param1, param2 > struct trait<sp1,sp2> \
-    : ::boost::type_traits::bool_trait< C > \
+    : mpl::bool_c<C> \
 { \
-    typedef ::boost::type_traits::bool_trait< C > base_trait_; \
-    BOOST_STATIC_CONSTANT(bool, value = base_trait_::value); \
+    BOOST_TT_AUX_BOOL_TRAIT_VALUE_DECL(C) \
 }; \
 /**/
 
