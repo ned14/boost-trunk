ecbdf0982476c65031f669714b8303791c9bcc24
agurtovoy <agurtovoy@localhost>
2004-01-25 14:31:41 +0000

    new vector implementation

---
+++ boost/boost/mpl/aux_/config/typeof.hpp
@@ -0,0 +1,30 @@
+
+#ifndef BOOST_MPL_AUX_CONFIG_TYPEOF_HPP_INCLUDED
+#define BOOST_MPL_AUX_CONFIG_TYPEOF_HPP_INCLUDED
+
+// Copyright (c) 2003-04 Aleksey Gurtovoy
+//
+// Use, modification and distribution are subject to the Boost Software 
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy 
+// at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/mpl for documentation.
+
+// $Source$
+// $Date$
+// $Revision$
+
+#include "boost/mpl/aux_/config/workaround.hpp"
+#include "boost/mpl/aux_/config/gcc.hpp"
+#include "boost/config.hpp"
+
+#if !defined(BOOST_MPL_CFG_TYPEOF_SEQUENCES) \
+    && (   defined(BOOST_MPL_CFG_GCC) && BOOST_MPL_CFG_GCC >= 0x0302 \
+        || defined(__MWERKS__) && __MWERKS__ >= 0x3000 \
+        )
+
+#   define BOOST_MPL_CFG_TYPEOF_BASED_SEQUENCES
+
+#endif
+
+#endif // BOOST_MPL_AUX_CONFIG_TYPEOF_HPP_INCLUDED
---
+++ boost/boost/mpl/distance_fwd.hpp
@@ -0,0 +1,32 @@
+
+#ifndef BOOST_MPL_DISTANCE_FWD_HPP_INCLUDED
+#define BOOST_MPL_DISTANCE_FWD_HPP_INCLUDED
+
+// Copyright (c) Aleksey Gurtovoy 2000-2004
+//
+// Use, modification and distribution are subject to the Boost Software 
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy 
+// at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/mpl for documentation.
+
+// $Source$
+// $Date$
+// $Revision$
+
+#include "boost/mpl/aux_/common_name_wknd.hpp"
+#include "boost/mpl/aux_/algorithm_namespace.hpp"
+
+namespace boost { namespace mpl {
+
+BOOST_MPL_AUX_AGLORITHM_NAMESPACE_BEGIN
+BOOST_MPL_AUX_COMMON_NAME_WKND(distance)
+
+template< typename First, typename Last > 
+struct distance;
+
+BOOST_MPL_AUX_AGLORITHM_NAMESPACE_END
+
+}} // namespace boost::mpl
+
+#endif // BOOST_MPL_DISTANCE_FWD_HPP_INCLUDED
