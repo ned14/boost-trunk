4dfe57fc76585457244df7c3c95ffee0024d98d0
djowel <djowel@localhost>
2004-07-23 02:50:33 +0000

    Spirit 1.6

--- boost/boost/spirit.hpp
+++ boost/boost/spirit.hpp
@@ -1,4 +1,5 @@
 /*=============================================================================
+    Spirit v1.6.0
     Copyright (c) 1998-2003 Joel de Guzman
     Copyright (c) 2001-2003 Daniel Nuffer
     Copyright (c) 2001-2003 Hartmut Kaiser
@@ -7,17 +8,18 @@
     Copyright (c) 2002 Raghavendra Satish
     Copyright (c) 2002 Jeff Westfahl
     Copyright (c) 2001 Bruce Florman
-    Copyright (c) 2003 Giovanni Bajo
-    Copyright (c) 2003 Vaclav Vesely
-    Copyright (c) 2003 Jonathan de Halleux
     http://spirit.sourceforge.net/
 
-    Use, modification and distribution is subject to the Boost Software
-    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-    http://www.boost.org/LICENSE_1_0.txt)
+    Permission to copy, use, modify, sell and distribute this software is
+    granted provided this copyright notice appears in all copies. This
+    software is provided "as is" without express or implied warranty, and
+    with no claim as to its suitability for any purpose.
 =============================================================================*/
 #if !defined(SPIRIT_HPP)
 #define SPIRIT_HPP
+
+#define SPIRIT_VERSION 0x1601
+#define SPIRIT_PIZZA_VERSION SPIRIT_PIZZA_EVERYTHING_EXCEPT_FISH  // :-)
 
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -25,48 +27,43 @@
 //  Spirit.Debug layer, otherwise the non-debug Spirit.Core is included.
 //
 ///////////////////////////////////////////////////////////////////////////////
-#include <boost/spirit/core.hpp>
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Spirit.Meta
-//
-///////////////////////////////////////////////////////////////////////////////
-#include <boost/spirit/meta.hpp>
+#include "boost/spirit/core.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  Spirit.ErrorHandling
 //
 ///////////////////////////////////////////////////////////////////////////////
-#include <boost/spirit/error_handling.hpp>
+#include "boost/spirit/error_handling.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  Spirit.Iterators
 //
 ///////////////////////////////////////////////////////////////////////////////
-#include <boost/spirit/iterator.hpp>
+#include "boost/spirit/iterator.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  Spirit.Symbols
 //
 ///////////////////////////////////////////////////////////////////////////////
-#include <boost/spirit/symbols.hpp>
+#include "boost/spirit/symbols.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  Spirit.Utilities
 //
 ///////////////////////////////////////////////////////////////////////////////
-#include <boost/spirit/utility.hpp>
+#include "boost/spirit/utility.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  Spirit.Attributes
 //
 ///////////////////////////////////////////////////////////////////////////////
-#include <boost/spirit/attribute.hpp>
+#if !defined(BOOST_MSVC) || (BOOST_MSVC > 1300)
+#include "boost/spirit/attribute.hpp"
+#endif
 
 #endif // !defined(SPIRIT_HPP)
