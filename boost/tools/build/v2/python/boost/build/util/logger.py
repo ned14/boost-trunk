5d75973d1cca80887e382dbc1d896892aa48ffda
pedro_ferreira <pedro_ferreira@localhost>
2005-02-07 19:40:10 +0000

    Changed to filter logs based on the source name rather than the source instance.

--- boost/tools/build/v2/python/boost/build/util/logger.py
+++ boost/tools/build/v2/python/boost/build/util/logger.py
@@ -8,8 +8,8 @@ class NullLogger:
     def __init__ (self):
         self.indent_ = ''
     
-    def log (self, source, *args):
-        if self.on () and self.interesting (source):
+    def log (self, source_name, *args):
+        if self.on () and self.interesting (source_name):
             self.do_log (self.indent_)
             for i in args:
                 self.do_log (i)
@@ -26,7 +26,7 @@ class NullLogger:
     def do_log (self, *args):
         pass
     
-    def interesting (self, source):
+    def interesting (self, source_name):
         return False
 
     def on (self):
@@ -39,7 +39,7 @@ class TextLogger (NullLogger):
     def do_log (self, arg):
             sys.stdout.write (str (arg))
     
-    def interesting (self, source):
+    def interesting (self, source_name):
         return True
 
     def on (self):
