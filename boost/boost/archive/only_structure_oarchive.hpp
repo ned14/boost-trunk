21666905f6ec81bee46cc1d7a3ac8ae5b7c7942d
troyer <troyer@localhost>
2005-09-25 15:03:11 +0000

    *** empty log message ***

---
+++ boost/boost/archive/only_structure_oarchive.hpp
@@ -0,0 +1,58 @@
+#ifndef BOOST_ARCHIVE_ONLY_STRUCTURE_OARCHIVE_HPP
+#define BOOST_ARCHIVE_ONLY_STRUCTURE_OARCHIVE_HPP
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+# pragma once
+#endif
+
+/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
+// only_structure_iarchive.hpp
+
+// (C) Copyright 2005 Matthias Troyer 
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org for updates, documentation, and revision history.
+
+#include <boost/archive/detail/auto_link_archive.hpp>
+#include <boost/archive/basic_archive.hpp>
+#include <boost/archive/fast_array_serialization.hpp>
+#include <boost/archive/ignore_oprimitive.hpp>
+#include <boost/archive/forward_structure_oarchive.hpp>
+#include <boost/archive/impl/archive_pointer_oserializer.ipp>
+
+#include <boost/archive/detail/abi_prefix.hpp> // must be the last header
+
+namespace boost { 
+namespace archive {
+
+// an archive wrapper that stores only the special types defined used by the serialization library
+// to define the data structures (classes, pointers, container sizes, ...)
+
+template <class ImplementationArchive>
+class only_structure_oarchive 
+  : public ignore_oprimitive,
+    public forward_structure_oarchive<only_structure_oarchive<ImplementationArchive>,ImplementationArchive >
+{
+public:
+    only_structure_oarchive(ImplementationArchive& ar) 
+	  : forward_structure_oarchive<only_structure_oarchive<ImplementationArchive>,ImplementationArchive >(ar)
+	{
+	}
+};
+
+template <class Type, class ImplementationArchive>
+struct fast_array_serialization<only_structure_oarchive<ImplementationArchive>,Type>
+  : public is_fundamental<Type>
+{};
+
+
+
+} // namespace archive
+} // namespace boost
+
+#include <boost/archive/detail/abi_suffix.hpp> // pops abi_suffix.hpp pragmas
+
+#endif // BOOST_ARCHIVE_ONLY_STRUCTURE_OARCHIVE_HPP
---
+++ boost/boost/archive/supports_typemap_serialization.hpp
@@ -0,0 +1,128 @@
+// (C) Copyright 2005 Matthias Troyer . 
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org for updates, documentation, and revision history.
+
+#ifndef BOOST_ARCHIVE_SUPPORTS_TYPEMAP_SERIALIZATION_HPP
+#define BOOST_ARCHIVE_SUPPORTS_TYPEMAP_SERIALIZATION_HPP
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+# pragma once
+#endif
+
+// This header is used to define archive traits to customize serialization
+
+#include <boost/config.hpp>
+
+#include <boost/mpl/bool.hpp>
+#include <boost/type_traits/is_fundamental.hpp>
+#include <boost/type_traits/is_pointer.hpp>
+#include <boost/type_traits/is_array.hpp>
+#include <boost/type_traits/remove_pointer.hpp>
+#include <boost/type_traits/remove_extent.hpp>
+#include <boost/mpl/if.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <vector>
+#include <deque>
+#include <list>
+
+#ifdef BOOST_HAS_SLIST
+#include <slist>
+#endif
+
+#if defined(__SGI_STL_PORT) || defined(_STLPORT_VERSION)
+#define STD _STLP_STD
+#else
+#define STD BOOST_STD_EXTENSION_NAMESPACE
+#endif
+
+
+namespace boost {
+namespace archive {
+
+/// @brief Traits to check for typemap serialization
+///
+/// Typemap serialization is a two-stage process, to support, e.g. MPI serialization without copying.
+///
+/// Upon deserialization, in a first stage, the memory for all objects is allocated using only
+/// the "structure" information from an archive, that is only the special types of the serialization
+/// library are deserialized, such as class ids, pointers, and size types of containers. That information
+/// has to be sufficient to allocate all the memory
+///
+/// In a second stage, the serialization library is used again, to register the address of all data members,
+/// thus building a memory map of the data structures, which is then used to fill the data members. This
+/// stage requires that serialization is done by serializing all members directly, and not temporaries.
+///
+/// Typemap serialization hence allows only a subset of the serialization functionalities. Serialization
+/// has to proceed by serializing all data members directly, and the objects must be constructible only
+/// using information serialized through the special types of the serialization library
+
+template <class Archive, class Type>
+struct supports_typemap_serialization;
+
+template <class Archive, class Type>
+struct supports_typemap_serialization
+ : public mpl::if_
+            < 
+			  is_pointer<Type>,
+			  supports_typemap_serialization<Archive,typename remove_pointer<Type>::type>,
+			  mpl::if_
+                <
+				  is_array<Type>,
+				  supports_typemap_serialization<Archive,typename remove_extent<Type>::type>,
+			      is_fundamental<Type>
+				>
+			>::type
+{};
+
+// specialization for std::vector
+
+template <class Archive, class U, class Allocator>
+struct supports_typemap_serialization<Archive, STD::vector<U,Allocator> >
+ : public supports_typemap_serialization<Archive,U>
+{};
+
+template <class Archive, class Allocator>
+struct supports_typemap_serialization<Archive, STD::vector<bool,Allocator> >
+ : public mpl::bool_<false>
+{};
+
+// specialization for std::list
+
+template <class Archive, class U, class Allocator>
+struct supports_typemap_serialization<Archive, STD::list<U,Allocator> >
+ : public supports_typemap_serialization<Archive,U>
+{};
+
+
+// specialization for std::deque
+
+template <class Archive, class U, class Allocator>
+struct supports_typemap_serialization<Archive, STD::deque<U,Allocator> >
+ : public supports_typemap_serialization<Archive,U>
+{};
+
+// specialization for atd::slist
+
+#ifdef BOOST_HAS_SLIST
+template <class Archive, class U, class Allocator>
+struct supports_typemap_serialization<Archive, STD::slist<U,Allocator> >
+ : public supports_typemap_serialization<Archive,U>
+{};
+#endif
+
+// specialization for boost::shared_ptr
+
+template <class Archive, class T>
+struct supports_typemap_serialization<Archive, boost::shared_ptr<T> >
+ : public supports_typemap_serialization<Archive,T>
+{};
+
+} // namespace archive
+} // namespace boost
+
+#endif // BOOST_ARCHIVE_SUPPORTS_TYPEMAP_SERIALIZATION_HPP
