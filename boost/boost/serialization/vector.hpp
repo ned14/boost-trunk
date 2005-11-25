02a18f2ced48c9d50930ed08d69fa4b8576a8416
troyer <troyer@localhost>
2005-11-25 16:31:46 +0000

    *** empty log message ***

--- boost/boost/serialization/vector.hpp
+++ boost/boost/serialization/vector.hpp
@@ -26,14 +26,6 @@
 #include <boost/serialization/collections_load_imp.hpp>
 #include <boost/serialization/split_free.hpp>
 
-#ifdef BOOST_ARRAY_SLIGHTLY_INTRUSIVE
-#include <boost/type_traits/has_trivial_constructor.hpp>
-#include <boost/type_traits/has_trivial_destructor.hpp>
-#include <boost/mpl/and.hpp>
-#include <boost/mpl/bool.hpp>
-#include <boost/serialization/detail/get_data.hpp>
-#endif
-
 // function specializations must be defined in the appropriate
 // namespace - boost::serialization
 #if defined(__SGI_STL_PORT) || defined(_STLPORT_VERSION)
@@ -48,8 +40,6 @@ namespace serialization {
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 // vector<T>
 
-
-#ifndef BOOST_ARRAY_SLIGHTLY_INTRUSIVE
 template<class Archive, class U, class Allocator>
 inline void save(
     Archive & ar,
@@ -60,74 +50,6 @@ inline void save(
         ar, t
     );
 }
-#else
-// if the value type is not trivially constructable or destructable, then
-// we have to use save_collection
-
-template<class Archive, class U, class Allocator>
-inline void save_optimized(
-    Archive & ar,
-    const STD::vector<U, Allocator> &t,
-    const unsigned int /* file_version */,
-    mpl::false_
-){
-    boost::serialization::stl::save_collection<Archive, STD::vector<U, Allocator> >(
-        ar, t
-    );
-}
-
-// if the value type is trivially constructable and destructable, then
-// we can use either the default save_array function or an optimized overload
-
-template<class Archive, class U, class Allocator>
-inline void save_optimized(
-    Archive & ar,
-    const STD::vector<U, Allocator> &t,
-    const unsigned int file_version,
-    mpl::true_
-){
-    const boost::archive::container_size_type count(t.size());
-    ar << BOOST_SERIALIZATION_NVP(count);
-    save_array(ar,detail::get_data(t),t.size(),file_version);
-}
-
-// dispatch the save depending on whether save_collection or save_array should be used
-template<class Archive, class U, class Allocator>
-inline void save(
-    Archive & ar,
-    STD::vector<U, Allocator> const &t,
-    const unsigned int file_version
-)
-{
-    save_optimized(
-        ar,t,file_version,
-        typename mpl::and_<has_trivial_constructor<U>, has_trivial_destructor<U> >::type()
-      );   
-}
-#endif
-
-// if the value type is not trivially constructable or destructable, then
-// we have to use load_collection
-
-template<class Archive, class U, class Allocator>
-inline void load_optimized(
-    Archive & ar,
-    STD::vector<U, Allocator> &t,
-    const unsigned int /* file_version */,
-    mpl::false_
-){
-    boost::serialization::stl::load_collection<
-        Archive,
-        STD::vector<U, Allocator>,
-        boost::serialization::stl::archive_input_seq<
-            Archive, STD::vector<U, Allocator> 
-        >,
-        boost::serialization::stl::reserve_imp<STD::vector<U, Allocator> >
-    >(ar, t);
-}
-
-
-#ifndef BOOST_ARRAY_SLIGHTLY_INTRUSIVE
 
 template<class Archive, class U, class Allocator>
 inline void load(
@@ -144,42 +66,6 @@ inline void load(
         boost::serialization::stl::reserve_imp<STD::vector<U, Allocator> >
     >(ar, t);
 }
-
-#else
-
-// if the value type is trivially constructable and destructable, then
-// we can use either the default load_array function or an optimized overload
-
-template<class Archive, class U, class Allocator>
-inline void load_optimized(
-    Archive & ar,
-    STD::vector<U, Allocator> &t,
-    const unsigned int file_version,
-    mpl::true_
-){
-    t.clear();
-    // retrieve number of elements
-    boost::archive::container_size_type count;
-    ar >> BOOST_SERIALIZATION_NVP(count);
-    t.resize(count);
-    serialization::load_array(ar,detail::get_data(t),t.size(),file_version);
-}
-
-// dispatch the load depending on whether load_collection or load_array should be used
-template<class Archive, class U, class Allocator>
-inline void load(
-    Archive & ar,
-    STD::vector<U, Allocator> &t,
-    const unsigned int file_version
-)
-{
-    load_optimized(
-        ar,t,file_version,
-        typename mpl::and_<has_trivial_constructor<U>, has_trivial_destructor<U> >::type()
-      );   
-}
-
-#endif
 
 // split non-intrusive serialization function member into separate
 // non intrusive save/load member functions
