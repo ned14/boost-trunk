6bdc2b65ed561781a1057193aa505c9a7e2b4c4d
djowel <djowel@localhost>
2006-10-11 23:55:02 +0000

    added multithreading test

--- boost/libs/spirit/phoenix/test/scope/dynamic_tests.cpp
+++ boost/libs/spirit/phoenix/test/scope/dynamic_tests.cpp
@@ -15,38 +15,47 @@
 #include <boost/spirit/phoenix/operator.hpp>
 #include <boost/spirit/phoenix/scope/dynamic.hpp>
 
-struct my_closure : boost::phoenix::dynamic_scope<int, std::string, double>
+struct my_dynamic : ::boost::phoenix::dynamic<int, std::string, double>
 {
-    my_closure() : num(*this), message(*this), real(*this) {}
+    my_dynamic() : num(*this), message(*this), real(*this) {}
 
     member1 num;
     member2 message;
     member3 real;
 };
 
+//  You may also use the macro below to achieve the same as above:
+//
+//  PHOENIX_DYNAMIC(
+//      my_dynamic,
+//          (int, num)
+//          (std::string, message)
+//          (double, real)
+//  );
+
 int
 main()
 {
     using namespace boost::phoenix;
 
-    my_closure clos;
+    my_dynamic clos;
 
     {   //  First stack frame
-        dynamic_frame<my_closure::self_type> frame(clos);
+        dynamic_frame<my_dynamic::self_type> frame(clos);
         (clos.num = 123)();
         (clos.num += 456)();
         (clos.real = clos.num / 56.5)();
         (clos.message = "Hello " + std::string("World "))();
 
         {   //  Second stack frame
-            dynamic_frame<my_closure::self_type> frame(clos);
+            dynamic_frame<my_dynamic::self_type> frame(clos);
             (clos.num = 987)();
             (clos.message = "Abracadabra ")();
             (clos.real = clos.num * 1e30)();
 
             {   //  Third stack frame
                 boost::fusion::vector<int, char const*, double> init(-1, "Direct Init ", 3.14);
-                dynamic_frame<my_closure::self_type> frame(clos, init);
+                dynamic_frame<my_dynamic::self_type> frame(clos, init);
 
                 (std::cout << clos.message << clos.num << ", " << clos.real << '\n')();
                 BOOST_TEST(clos.num() == -1);
