20a5b7ffcb4f32af5231aedcb6735e555e9b24e5
hkaiser <hkaiser@localhost>
2006-10-11 15:08:15 +0000

    Updated Spirit version and pizza information.

--- boost/boost/spirit/version.hpp
+++ boost/boost/spirit/version.hpp
@@ -1,5 +1,5 @@
 /*=============================================================================
-    Copyright (c) 2001-2003 Hartmut Kaiser
+    Copyright (c) 2001-2006 Hartmut Kaiser
     http://spirit.sourceforge.net/
 
     Use, modification and distribution is subject to the Boost Software
@@ -11,13 +11,13 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  This checks, whether the used Boost library is at least V1.32.0
+//  This checks, whether the used Boost library is at least V1.34.0
 //
 ///////////////////////////////////////////////////////////////////////////////
 #include <boost/version.hpp>
 
-#if BOOST_VERSION < 103200
-#error "Spirit v1.8.x needs at least Boost V1.32.0 to compile successfully."
+#if BOOST_VERSION < 103400
+#error "Spirit v1.9.x needs at least Boost V1.34.0 to compile successfully."
 #endif 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -25,7 +25,7 @@
 //  This is the version of the current Spirit distribution
 //
 ///////////////////////////////////////////////////////////////////////////////
-#define SPIRIT_VERSION 0x1803
-#define SPIRIT_PIZZA_VERSION SPIRIT_DOUBLE_CHEESE  // :-)
+#define SPIRIT_VERSION 0x1900
+#define SPIRIT_PIZZA_VERSION SPIRIT_GERMAN_SAUCAGE  // :-)
 
 #endif // defined(SPIRIT_VERSION_HPP)
