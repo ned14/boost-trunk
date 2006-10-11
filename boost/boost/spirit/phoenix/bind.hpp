7e023365e4112892d2aaf126596e63b709d7627d
djowel <djowel@localhost>
2006-10-11 09:44:27 +0000

    move to phoenix2

---
+++ boost/boost/spirit/phoenix/bind.hpp
@@ -0,0 +1,17 @@
+/*=============================================================================
+    Copyright (c) 2001-2006 Joel de Guzman
+
+    Use, modification and distribution is subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+#ifndef PHOENIX_BIND_HPP
+#define PHOENIX_BIND_HPP
+
+#include <boost/spirit/phoenix/version.hpp>
+#include <boost/spirit/phoenix/bind/bind_function.hpp>
+#include <boost/spirit/phoenix/bind/bind_function_object.hpp>
+#include <boost/spirit/phoenix/bind/bind_member_function.hpp>
+#include <boost/spirit/phoenix/bind/bind_member_variable.hpp>
+
+#endif
