28c71794e5381c90b097b8d0ff2f5719711e127e
vladimir_prus <vladimir_prus@localhost>
2007-10-14 18:13:58 +0000

    Working on build_system.jam

---
+++ python_port/python/boost/build/build_system.py
@@ -0,0 +1,7 @@
+# Status: being ported by Vladimir Prus.
+
+# Copyright 2003, 2005 Dave Abrahams 
+# Copyright 2006 Rene Rivera 
+# Copyright 2003, 2004, 2005, 2006 Vladimir Prus 
+# Distributed under the Boost Software License, Version 1.0. 
+# (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt) 
